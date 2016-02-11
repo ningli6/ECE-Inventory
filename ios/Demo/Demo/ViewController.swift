531fd6ea4302b1e5aebff72423006d8f26200f0b
ningli <ningli@vt.edu>
2016-02-11 14:35:07 -0500

    Dismiss keyboard

--- ios/Demo/Demo/ViewController.swift
+++ ios/Demo/Demo/ViewController.swift
@@ -20,6 +20,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
+    }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
 
     override func didReceiveMemoryWarning() {
