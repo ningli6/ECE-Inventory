d4f40a31f6f382d99a5f2b03326611c526022ff5
Ning Li <ningli@vt.edu>
2016-04-05 18:02:29 -0400

    Implement ownership transfer in the backend

---
+++ backend/ECEInventory/ECEInventory/Controllers/ItemsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ECEInventory.Models;
+
+namespace ECEInventory.Controllers
+{
+    public class ItemsController : ApiController
+    {
+        private InventoryDBContext db = new InventoryDBContext();
+
+        //// GET: api/Items
+        //public IQueryable<Item> GetItems()
+        //{
+        //    return db.Items;
+        //}
+
+        //// GET: api/Items/5
+        //[ResponseType(typeof(Item))]
+        //public async Task<IHttpActionResult> GetItem(int id)
+        //{
+        //    Item item = await db.Items.FindAsync(id);
+        //    if (item == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return Ok(item);
+        //}
+
+        /// <summary>
+        /// Get item by barcode
+        /// </summary>
+        /// <param name="barcode"></param>
+        /// <returns></returns>
+        [Route("api/Items/{barcode}")]
+        public IQueryable<Item> GetItemByBarcode(string barcode)
+        {
+            return db.Items.Where(item => item.Ptag == barcode);
+        }
+
+        /// <summary>
+        /// Get items by username/id/pid/email
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [Route("api/Users/{user}")]
+        public IQueryable<Item> GetItemsByUser(string user)
+        {
+            return db.Items.Where(item => item.Custodian == user);
+        }
+
+        /// <summary>
+        /// Get item ownership and location history
+        /// </summary>
+        /// <param name="barcode"></param>
+        /// <returns></returns>
+        [Route("api/Histories/{barcode}")]
+        public IQueryable<History> GetItemHistory(string barcode)
+        {
+            return db.Histories.Where(record => record.Ptag == barcode).OrderByDescending(record => record.Time);
+        }
+
+        //// PUT: api/Items/5
+        //[ResponseType(typeof(void))]
+        //public async Task<IHttpActionResult> PutItem(int id, Item item)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    if (id != item.Id)
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    db.Entry(item).State = EntityState.Modified;
+
+        //    try
+        //    {
+        //        await db.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!ItemExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+
+        //    return StatusCode(HttpStatusCode.NoContent);
+        //}
+
+        //// POST: api/Items
+        //[ResponseType(typeof(Item))]
+        //public async Task<IHttpActionResult> PostItem(Item item)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    db.Items.Add(item);
+        //    await db.SaveChangesAsync();
+
+        //    return CreatedAtRoute("DefaultApi", new { pTag = item.Ptag }, item);
+        //}
+
+        //// DELETE: api/Items/5
+        //[ResponseType(typeof(Item))]
+        //public async Task<IHttpActionResult> DeleteItem(int id)
+        //{
+        //    Item item = await db.Items.FindAsync(id);
+        //    if (item == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    db.Items.Remove(item);
+        //    await db.SaveChangesAsync();
+
+        //    return Ok(item);
+        //}
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
+
+        //private bool ItemExists(int id)
+        //{
+        //    return db.Items.Count(e => e.Id == id) > 0;
+        //}
+    }
+}
---
+++ backend/ECEInventory/ECEInventory/Controllers/TransfersViewController.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ECEInventory.Models;
+
+// this controller is for mvc
+
+namespace ECEInventory.Controllers
+{
+    public class TransfersViewController : Controller
+    {
+        private InventoryDBContext db = new InventoryDBContext();
+
+        /// <summary>
+        /// Get unprocessed transfer requests
+        /// </summary>
+        /// <returns></returns>
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Transfers.Where(t => t.Status == 0).ToListAsync());
+        }
+
+        /// <summary>
+        /// Get all transfer requests
+        /// </summary>
+        /// <returns></returns>
+        public async Task<ActionResult> AllRequests()
+        {
+            return View(await db.Transfers.ToListAsync());
+        }
+
+        /// <summary>
+        /// If the request was approved, also need to update transfer history
+        /// </summary>
+        /// <param name="barcode"></param>
+        /// <returns></returns>
+        public async Task<ActionResult> Approve(string barcode)
+        {
+            if (barcode == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Transfer transfer = await db.Transfers.FindAsync(barcode);
+            if (transfer == null)
+            {
+                return HttpNotFound();
+            }
+            // update transfer status
+            transfer.Status = 1;
+
+            // update the status in database
+            db.Transfers.Attach(transfer);
+            var entry = db.Entry(transfer);
+            entry.Property(e => e.Status).IsModified = true;
+
+            Item item = await db.Items.FindAsync(barcode);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            // update the custodian
+            item.Custodian = transfer.Receiver;
+
+            // get a new history with new custodian (item was updated)
+            History record = new History(item);
+            record.Time = transfer.Time;
+
+            // update existing entry in item table
+            db.Items.Attach(item);
+            var itemEntry = db.Entry(item);
+            itemEntry.Property(e => e.Custodian).IsModified = true;
+            
+            // save the new record to history
+            db.Histories.Add(record);
+
+            // save all changes
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// If the request was denied, do nothing
+        /// </summary>
+        /// <param name="barcode"></param>
+        /// <returns></returns>
+        public async Task<ActionResult> Deny(string barcode)
+        {
+            if (barcode == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Transfer transfer = await db.Transfers.FindAsync(barcode);
+            if (transfer == null)
+            {
+                return HttpNotFound();
+            }
+            transfer.Status = 2;
+            db.Transfers.Attach(transfer);
+            var entry = db.Entry(transfer);
+            entry.Property(e => e.Status).IsModified = true;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET:  details in the pending request list
+        public async Task<ActionResult> Details(string barcode)
+        {
+            if (barcode == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = await db.Items.FindAsync(barcode);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // GET: details in the all request list
+        public async Task<ActionResult> AllRequestsDetails(string barcode)
+        {
+            if (barcode == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = await db.Items.FindAsync(barcode);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        //// GET: TransfersView/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: TransfersView/Create
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> Create([Bind(Include = "Ptag,Sender,Receiver,Status,Time")] Transfer transfer)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Transfers.Add(transfer);
+        //        await db.SaveChangesAsync();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View(transfer);
+        //}
+
+        //// GET: TransfersView/Edit/5
+        //public async Task<ActionResult> Edit(string id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Transfer transfer = await db.Transfers.FindAsync(id);
+        //    if (transfer == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(transfer);
+        //}
+
+        //// POST: TransfersView/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> Edit([Bind(Include = "Ptag,Sender,Receiver,Status,Time")] Transfer transfer)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(transfer).State = EntityState.Modified;
+        //        await db.SaveChangesAsync();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(transfer);
+        //}
+
+        //// GET: TransfersView/Delete/5
+        //public async Task<ActionResult> Delete(string id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Transfer transfer = await db.Transfers.FindAsync(id);
+        //    if (transfer == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(transfer);
+        //}
+
+        //// POST: TransfersView/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> DeleteConfirmed(string id)
+        //{
+        //    Transfer transfer = await db.Transfers.FindAsync(id);
+        //    db.Transfers.Remove(transfer);
+        //    await db.SaveChangesAsync();
+        //    return RedirectToAction("Index");
+        //}
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
+    }
+}
