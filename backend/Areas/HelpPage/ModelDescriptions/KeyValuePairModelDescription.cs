36c940cb72a982aecb1d337ba664fe6c2035a877
ningli <ningli@vt.edu>
2016-04-07 09:29:40 -0400

    Redundant files

--- backend/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace ECEInventory2.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
